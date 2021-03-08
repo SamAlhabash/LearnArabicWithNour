9404a05d507ee617a042b5489d4c2df3702b46e8
Isaam Alhabash <sam.alhabash@gmail.com>
2021-03-08 09:14:33 +0200

    Fixed Routing Module for Child

--- src/app/views/pages/excercises/excercises-routing.module.ts
+++ src/app/views/pages/excercises/excercises-routing.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ExcerciseComponent } from './excercise/excercise.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ExcerciseComponent } from "./excercise/excercise.component";
 
-import { ExcercisesComponent } from './excercises.component';
+import { ExcercisesComponent } from "./excercises.component";
 
-const routes: Routes = [{ path: 'excercises', component: ExcerciseComponent },
-{path: 'excercises/:name', component: ExcerciseComponent}];
+const routes: Routes = [
+  {
+    path: "",
+    component: ExcercisesComponent,
+    children: [
+      {
+        path: "",
+        component: ExcerciseComponent
+      }
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class ExcercisesRoutingModule {
-  
- }
+export class ExcercisesRoutingModule {}
